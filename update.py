e35087054e3da0cf69b0af5c09079eee716f5e57
unkn0wn_bali <40415158+unkn0wnh4ckr@users.noreply.github.com>
2019-05-15 16:54:54 +0100

    Add files via upload

---
+++ update.py
@@ -0,0 +1,4 @@
+import os
+print "UPDATING..."
+os.system("cd")
+os.system('cd /root/ && rm -fr hackers-tool-kit && git clone https://github.com/unkn0wnh4ckr/hackers-tool-kit && echo "[UPDATED]:  Restart Your Terminal"')
